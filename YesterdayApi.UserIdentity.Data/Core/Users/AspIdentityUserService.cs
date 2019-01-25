efe6b022724922a1a73fe09070a41f42fd6e1837
Alex Toryanik <alex1toryanik@gmail.com>
2019-01-25 11:39:31 +0100

    feature(security) add the needed methods in AspIdentityUserService

--- YesterdayApi.UserIdentity.Data/Core/Users/AspIdentityUserService.cs
+++ YesterdayApi.UserIdentity.Data/Core/Users/AspIdentityUserService.cs
@@ -1,6 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YesterdayApi.UserIdentity.Data.Infrastructure.Database;
+
 namespace YesterdayApi.UserIdentity.Data.Core.Users
 {
-    class AspIdentityUserService
+    public class AspIdentityUserService : IAspIdentityUserService
     {
+        private readonly UserDbContext _context;
+
+        public AspIdentityUserService(UserDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<AspIdentityUser> GetById(int id)
+        {
+            if (await UserExists(id))
+                return await _context.Users.FindAsync(id).ConfigureAwait(false);
+            return null;
+        }
+
+        private Task<bool> UserExists(int id)
+        {
+            return _context.Users.AnyAsync(x => x.Id.Equals(id));
+        }
     }
 }
